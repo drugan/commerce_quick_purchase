b7c5c69f11333a9f1ba7966cbaa3eb30eae0e6f9
drugan <drugan@1578644.no-reply.drupal.org>
2020-09-10 17:59:44 +0300

    Issue #3170270 by drugan: Replace deprecated commerce.availability_manager service

--- src/Form/QuickPurchaseAddToCartForm.php
+++ src/Form/QuickPurchaseAddToCartForm.php
@@ -251,13 +251,13 @@ class QuickPurchaseAddToCartForm extends AddToCartForm {
           ];
 
           if ($available = $purchased_entity) {
-            $availability = \Drupal::service('commerce.availability_manager');
+            $availability = \Drupal::service('commerce_order.availability_manager');
             $context = new Context($this->currentUser, $this->selectStore($purchased_entity), time(), [
               'xquantity' => 'add_to_cart',
             ]);
-            $available = $availability->check($purchased_entity, $default_quantity, $context);
+            $available = $availability->check($purchased_entity, $context, $default_quantity);
             if (!$available && method_exists($order_item, 'rotateStock') && $order_item->rotateStock($purchased_entity, $default_quantity, $context)) {
-              $available = $availability->check($purchased_entity, $default_quantity, $context);
+              $available = $availability->check($purchased_entity, $context, $default_quantity);
             }
           }
           if (!$available) {
